6dd9585dc72ee9aa4f041af9d7536142d32df712
Susanna Alvkvist <susannaalvkvist@hotmail.com>
2023-03-03 22:36:29 +0100

    Add project files.

---
+++ KassaSystem/App.cs
@@ -0,0 +1,150 @@
+﻿using static System.Runtime.InteropServices.JavaScript.JSType;
+
+internal class App : AppBase
+{
+    public void Run()
+    {
+
+        KassaMeny();
+
+    }
+
+    //måste fixa kvitto spar separerar för varje månad
+    // ska finnas artiklar datum pris och total pris
+    //PAY = klar med köpet
+
+   
+
+
+
+   
+
+
+    public void KassaMeny()
+    {
+        bool isRunning = true;
+
+        while (isRunning)
+        {
+
+            Console.WriteLine("================");
+            Console.WriteLine("===   Kassa  ===");
+            Console.WriteLine("================");
+            Console.WriteLine("1. Ny kund");
+            Console.WriteLine("0. Avsluta");
+            Console.WriteLine("2. Admin");
+
+
+            int sel = CheckNumber(0, 1);
+
+            switch (sel)
+            {
+
+                case 1:
+                    Kassa();
+                    break;
+
+                    case 2:
+                 //   Admin(); skapa en admin vy.
+                    break;
+
+                case 0:
+                    isRunning = false;
+                    break;
+                default:
+                    sel = Convert.ToInt32(Console.ReadLine());
+                    if (sel < 0 && sel < 2)
+                    {
+                        Console.WriteLine("Felaktigt val! Mata in igen");
+                        sel = Convert.ToInt32(Console.ReadLine());
+                    }
+                    break;
+
+            }
+
+
+        }
+    }
+
+    public void Kassa()
+    {
+
+
+        Console.Clear();
+        Console.WriteLine("KASSA");
+        string input = Console.ReadLine();
+        string[] inputs = input.Split(' ');
+
+        foreach (string inp in inputs)
+        {
+            Kvitto();
+        }
+
+        //meningen är att den ska skriva ut det man lagt in och räkna ut hur mycket det blir
+
+
+        Console.Read();
+    }
+
+
+
+    private void OrderTotal()
+    {
+        //här inne ska själva funktionen för att räkna ihop alla värderna finnas
+
+        // produkt * antal = 
+    }
+
+
+
+    private void Kvitto()
+    {
+        var date = DateTime.Now;
+
+        Console.WriteLine("Kvitto");
+        Console.WriteLine($"Datum: {date.ToLocalTime()}");
+        Console.WriteLine("Ordern"); // här skrivs alla produkter ut !
+        Console.WriteLine($"Total: {OrderTotal} kr");
+        // Här ska produkterna valda skrivas in  och antal med pris
+    }
+
+
+    public int CheckNumber(int min, int max)
+    {
+        int tal = 0;
+
+        while (true)
+        {
+
+            if (int.TryParse(Console.ReadLine(), out tal) == false)
+            {
+
+                Console.WriteLine("Felaktig inmatning, ange ett tal");
+                continue;
+            }
+
+            return tal;
+        }
+    }
+
+    public bool CheckString()
+    {
+        while (true)
+        {
+            string pay = Console.ReadLine().ToLower();
+
+            if (pay == "pay")
+            {
+                Console.Clear();
+                Kvitto();
+                return true;
+
+            }
+              else
+                return false;
+            
+        }
+    }
+
+
+}
---
+++ KassaSystem/Produkter.cs
@@ -0,0 +1,35 @@
+﻿using System.Runtime.CompilerServices;
+
+public class Produkter
+{
+
+ // Produktlista här med pris
+
+   
+
+ 
+
+
+
+    //här borde en funktion med att räkna ut produkterna finnas kanske ?
+
+
+
+}
+
+
+//public class ProduktList
+//{
+//    string banan = "901";
+//    string äpple = "902";
+//    string mjölk = "903";
+//    string bröd = "904";
+//    string kaffe = "905";
+//    string blöjor = "906";
+//    string cola = "907";
+//    string choklad = "908";
+//    string toapapper = "909";
+//    string tvättmedel = "910";
+//// det ska ha pris pch kunna gångras med antal. kanske göra en array ist ?
+
+//}
